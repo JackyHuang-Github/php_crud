71b5da327cb36263f324835f1909a49719380290
jacky58_huang <jacky58.huang@gmail.com>
2023-04-24 17:51:49 +0800

    1120424-1751

---
+++ resources/views/users_0423-2348/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('users._editbase')
+@section('content')
+<div class="card-header text-center">
+    <h4>會員管理系統 - 修改資料</h4>
+    <hr>
+</div>
+
+{{-- 傳遞 PUT 模式資訊給子頁面 _form --}}
+@section('methodMode')
+    @php
+        $_method = "PUT";
+    @endphp
+@show
+
+<form name="edit_form" action="{{ route('users.update', ['user' => $user]) }}" method="POST" enctype="multipart/form-data">
+    {{-- 設定 Action 為 PUT --}}
+    @method("PUT")
+    {{-- 設定防禦 CSRF 的攻擊 --}}
+    @csrf
+    @include('users._form') 
+</form>
+@endsection
---
+++ resources/views/users_0423-2348/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.rtl.min.css" integrity="sha384-5/ZcxA7Dub2FNG09dHw8CHmPN7Fz6ASlweagj0nuXjmMyupgH9n9F5Hd926zsu3/" crossorigin="anonymous">
+
+    <title>會員管理系統</title>
+
+    <style>
+        table tr th {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        table tr td {
+            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <form align=center action="{{ url('users/deleteSelected') }}" name='formMaster' method='POST'>
+            {{-- 設定防禦 CSRF 的攻擊 --}}
+            @csrf
+            <div>
+                <h3 align='center'><span class="text-danger">會 員 管 理 系 統</span></h3>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <a href="{{ url('users/create') }}" class="btn btn-primary btn-sm">新增會員</a>
+                    <a href='#' class="btn btn-primary btn-sm" onclick="del_selected();">刪除被選取資料</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    @yield('content')
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        function del_selected() {
+            if(confirm('是否確定要刪除這些被選取的資料？ 刪除後將無法恢復！')) {
+                formMaster.submit(); 
+            }
+            return false;
+        }
+    </script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
