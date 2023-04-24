9dcf420161ce8fee119c2d95489e3c604b95d5e5
JACKY-WIN11\JackyHuang <jacky58.huang@gmail.com>
2023-04-24 22:45:50 +0800

    1120424-2245

--- resources/views/users/master.blade.php
+++ resources/views/users/master.blade.php
@@ -20,27 +20,51 @@
     </style>
 </head>
 <body>
-    <div class="container-fluid">
-        <form align=center action="{{ url('users/deleteSelected') }}" name='formMaster' method='POST'>
-            {{-- 設定防禦 CSRF 的攻擊 --}}
-            @csrf
-            <div>
-                <h3 align='center'><span class="text-danger">會 員 管 理 系 統</span></h3>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <a href="{{ url('users/create') }}" class="btn btn-primary btn-sm">新增會員</a>
-                    <a href='#' class="btn btn-primary btn-sm" onclick="del_selected();">刪除被選取資料</a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    @yield('content')
-                </div>
-            </div>
-        </form>
+    <div class="container-fluid bg-secondary">
+        <div class="row">
+            <div class="col">
+                <form align=center action="{{ url('users/deleteSelected') }}" name='formMaster' method='POST'>
+                    {{-- 設定防禦 CSRF 的攻擊 --}}
+                    @csrf
+                    <div class="row mt-1">
+                        <div class="col">
+                            <h3 align='center'><span class="text-light">會 員 管 理 系 統</span></h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-2 ms-auto">
+                            <a href="{{ url('users/create') }}" class="btn btn-primary btn-sm">新增會員</a>
+                            <a href='#' class="btn btn-primary btn-sm" onclick="del_selected();">刪除被選取資料</a>
+                        </div>
+                        <div class="col-10">
+                            <ul class="pagination justify-content-end m-0 p-0">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            @yield('content')
+                        </div>
+                    </div>
+                </form>
+            </div>>
+        </div>
     </div>
-
     <script>
         function del_selected() {
             if(confirm('是否確定要刪除這些被選取的資料？ 刪除後將無法恢復！')) {
